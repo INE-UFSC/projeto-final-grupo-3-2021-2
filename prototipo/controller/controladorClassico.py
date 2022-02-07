0ad4811307c2eff2d7d2eb1a45ac39982d3f4e43
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2022-02-07 14:32:00 -0300

    Fix meia boca do problema com imports
    Ver controladorJogo.py

---
+++ prototipo/controller/controladorClassico.py
@@ -0,0 +1,9 @@
+import pygame
+from controladorJogo import ControladorJogo
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+
+class ControladorClassico(ControladorJogo):
+    pass
--- prototipo/controller/testes.py
+++ prototipo/controller/testes.py
@@ -1,23 +1,23 @@
-from turtle import width
-from controladorJogo import ControladorJogo
+from controladorClassico import ControladorClassico
 import pygame
 
 width = 1000
 height = 600
 whatever = {}
 
-pygame.init()
-c = ControladorJogo(width, height, whatever)
-c.rodar_jogo()
-
-
 def main():
+    
+    pygame.init()
+    c = ControladorClassico(width, height, whatever)
+    c.rodar_jogo()
+    pygame.display.update()
+    
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-        pygame.display.flip()
+        pygame.display.update()
 
 
 main()
