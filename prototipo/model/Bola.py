c42d2fb8d9136e8a69cd498a515697d6779416f0
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2022-02-08 15:44:51 -0300

    Mudança em campo.py e gerabola.py
    GeraBola cria e tbm coloca a bola na tela. Campo.campo guarda uma lista de objs Bola, agora com um novo atributo circle_obj (Rect do pygame), que é o elemento em tela

--- prototipo/model/Bola.py
+++ prototipo/model/Bola.py
@@ -1,19 +1,10 @@
 from abc import ABC
 
-
 class Bola(ABC):
 
-    def __init__(self, cor: str, raio: float):
-        self.__cor = cor
+    def __init__(self, raio: float, circle):
         self.__raio = raio
-
-    @property
-    def cor(self):
-        return self.__cor
-
-    @cor.setter
-    def cor(self, nova_cor: str):
-        self.__cor = nova_cor
+        self.__circle_obj = circle
 
     @property
     def raio(self):
@@ -22,3 +13,11 @@ class Bola(ABC):
     @raio.setter
     def raio(self, novo_raio: str):
         self.__raio = novo_raio
+        
+    @property
+    def circle_obj(self):
+        return self.__circle_obj
+
+    @circle_obj.setter
+    def circle_obj(self, novo_circle_obj: str):
+        self.__circle_obj = novo_circle_obj
--- prototipo/model/geraBola.py
+++ prototipo/model/geraBola.py
@@ -1,16 +1,26 @@
+from random import randint
 from campo import Campo
 from bolaMais import BolaMais
 from bolaMateriaNegra import BolaMateriaNegra
 from bolaMenos import BolaMenos
 from bolaBranca import BolaBranca
+from bolaNormal import BolaNormal
+import pygame
 
 
 class GeraBola():
 
     def __init__(self, campo: Campo) -> None:
-        self.__max_bola = 0
-        self.__min_bola = 0
+        self.__max_bola = 5
+        self.__min_bola = 1
         self.__campo = campo
         self.__lista_bola_especial = [
             BolaMais, BolaMenos, BolaBranca, BolaMateriaNegra
         ]
+
+    def geraBola(self, background, coors):
+        bola_img = pygame.draw.circle(background, "#A89234", coors, 35)
+        bola = BolaNormal(35, randint(
+            self.__min_bola, self.__max_bola), "He", bola_img)
+
+        return bola
