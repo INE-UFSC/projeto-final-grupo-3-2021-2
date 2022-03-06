bb8b9fa232f5d2c9a288075769461b13f7d8e6ae
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2022-03-06 14:40:36 -0300

    Bolas especiais
    Correção nas interfaces (ver BolaEspecial.py) e lógica básica para gerá-las em geraBola.py

--- prototipo/model/BolaBranca.py
+++ prototipo/model/BolaBranca.py
@@ -3,8 +3,8 @@ from model.BolaNormal import BolaNormal
 
 
 class BolaBranca(BolaEspecial):
-    def __init__(self, cor: str, raio: float, chance: float, em_campo: bool):
-        super().__init__(cor, raio, chance, em_campo)
+    def __init__(self, circle_obj, em_campo: bool):
+        super().__init__(circle_obj, em_campo)
 
     def acao(self, bola: BolaNormal):
         self.circle_obj = bola.circle_obj
